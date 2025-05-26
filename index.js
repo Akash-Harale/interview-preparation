717c069ae3666ff44ae5b0156db9927e8099a9d9
Akash <akash.v.harale@gmail.com>
2025-05-26 14:27:26 +0530

    callback hell

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ function calculate(a, b, callback) {
 
 const ans = calculate(5, 10, sum);
 console.log(ans);
+
+// Callback Hell
+
+const getData = (id, getNextData) => {
+  setTimeout(() => {
+    console.log(`data ${id}`);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 2000);
+};
+
+getData(1, () => {
+  getData(2, () => {
+    getData(3);
+  });
+});
